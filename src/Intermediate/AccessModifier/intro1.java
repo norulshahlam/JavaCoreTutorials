470a2eadd0ac525a0d9466c7e3127cd7ecadf85b
norulshahlam <norulshahlam@gmail.com>
2021-04-25 18:15:46 +0800

    g

--- src/Intermediate/AccessModifier/intro1.java
+++ src/Intermediate/AccessModifier/intro1.java
@@ -1,37 +1,12 @@
 package Intermediate.AccessModifier;
 
-/*
-Access Modifiers in Java
-
-There are two types of modifiers in Java: access modifiers and non-access modifiers.
-
-The access modifiers in Java specifies the accessibility or scope of a field, method, constructor, or class. We can change the access level of fields, constructors, methods, and class by applying the access modifier on it.
-
-There are four types of Java access modifiers:
-
-1. Private: The access level of a private modifier is only within the class. It cannot be accessed from outside the class.
-2. Default: The access level of a default modifier is only within the package. It cannot be accessed from outside the package. If you do not specify any access level, it will be the default.
-3. Protected: The access level of a protected modifier is within the package and outside the package through child class. If you do not make the child class, it cannot be accessed from outside the package.
-4. Public: The access level of a public modifier is everywhere. It can be accessed from within the class, outside the class, within the package and outside the package.
-
-There are many non-access modifiers, such as static, abstract, synchronized, native, volatile, transient, etc. Here, we are going to learn the access modifiers only.
-
- 						ACCESSIBILITY MATRIX
-						---------------------
-					within	within		outside package
-modifier	class		package		by subclass			other classes
-------------------------------------------------------
-private	  	yes			no		  	no				  		no
-default		  yes			yes		  	no	 				 		no
-protected 	yes			yes		  	yes					  	no
-public	  	yes			yes			  yes		 				 	yes
-
- */
-
 public class intro1 {
+
 	public static void main(String[] args) {
+		
 		protected1 new1 = new protected1();
-		System.out.println(new1.y);
+
+		System.out.println(new1.y);	// (1)
 		System.out.println(new1.z);
 
 		System.out.println("");
@@ -57,6 +32,7 @@ class protected1 {
 	protected int y = 5;
 	int z = 6;
 }
+@SuppressWarnings("unused")
 class private1 extends protected1 {
 
 	private int a = 1;
